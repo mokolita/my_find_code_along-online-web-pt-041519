436985888e1f1f94c9af3f33c8ecb1bc6ddee80a
Meg Galvez <meg@oakmaine.com>
2019-04-16 17:05:59 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-     return collectin[i] if yield(collectin[i])
+     return collection[i] if yield(collectin[i])
     end 
   i = i + 1 
 end 
